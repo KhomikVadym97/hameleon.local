551d5b2d09bbbcd66cbc79a0573a9747387fa539
Your Name <you@example.com>
2020-03-20 23:55:30 +0200

    Revert "initial commit"
    This reverts commit 4f05ca47ada3e65e525cad366d1d78f07f151ac5.

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,4 @@ const mix = require("laravel-mix");
 mix.js("resources/js/app.js", "public/js")
     .sass("resources/sass/app.scss", "public/css")
     .version()
-    .copyDirectory("resources/img/", "public/img");
+    .copyDirectory("resources/img", "public/img");
